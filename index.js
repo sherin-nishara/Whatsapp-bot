98d49eb55bef64e3fd1943e3272aeef315d410b6
sherin-nishara <sherinnishara@outlook.com>
2026-09-05 05:27:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const client = new Client({
 });
 
 client.on('qr', (qr) => {
-  console.log('📱 Scan this QR code:');
-  qrcode.generate(qr, { small: true });
+  console.log('📱 QR TEXT:');
+  console.log(qr);
 });
 
 client.on('authenticated', () => {
